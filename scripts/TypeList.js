09d573b69986eeb0732f4b0817678a04433b6241
PhiferTristan <phifer.tristan@outlook.com>
2023-08-03 09:36:40 -0500

    added html archetype list

--- scripts/TypeList.js
+++ scripts/TypeList.js
@@ -1,24 +1,32 @@
+import { getArchetypes } from "./database.js";
+
 document.addEventListener("click", (clickEvent) => {
   const typeClicked = clickEvent.target;
 
   if (typeClicked.dataset.type === "type") {
     const detailsHtml = `
     <div>
-      <h4>Greeting:</h4>
-      <h4>School:</h4>
-      <h4>Study Book:</h4>
+    <h4>Greeting:</h4>
+    <h4>School:</h4>
+    <h4>Study Book:</h4>
     </div>`;
   }
 });
 
 export const TypeList = () => {
+  const archetypes = getArchetypes();
+
   let headerHtml = `<div id="archetypes_container" class="content_block">`;
-  headerHtml += `<h3>Magician Types:</h3>`;
+
+  headerHtml += `<h3>Magician Types:</h3>
+                  <ol>`;
+
   for (const archetype of archetypes) {
-    headerHTML += `<li data-id="${archetype.id}"
-                        >${archetype.name}</li>`;
+    headerHtml += `<li data-type="archetype"
+                       data-id="${archetype.id}"
+                      >${archetype.name}</li>`;
   }
 
-  headerHtml += `</div>`;
+  headerHtml += `</ol></div>`;
   return headerHtml;
 };
