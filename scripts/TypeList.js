df11ef0bd50bc2167b5f6d05139f69f0e2af9d9c
PhiferTristan <phifer.tristan@outlook.com>
2023-08-04 13:56:12 -0500

    Added the book to be displayed in the study book details section

--- scripts/TypeList.js
+++ scripts/TypeList.js
@@ -1,22 +1,29 @@
-import { getArchetypes, getSchools } from "./database.js";
+import { getArchetypes, getSchools, getBooks } from "./database.js";
 
 const schools = getSchools();
+const books = getBooks();
 
 document.addEventListener("click", (clickEvent) => {
   const typeClicked = clickEvent.target;
 
   if (typeClicked.dataset.type === "archetype") {
     let archetypesSchool = "";
-    for (const school of schools){
-      if (parseInt(typeClicked.dataset.schoolid) === school.id){
-        archetypesSchool = school.name
+    let archetypeBook = "";
+    for (const school of schools) {
+      if (parseInt(typeClicked.dataset.schoolid) === school.id) {
+        archetypesSchool = school.name;
+      }
+    }
+    for (const book of books) {
+      if (parseInt(typeClicked.dataset.bookid) === book.id) {
+        archetypeBook = book.name;
       }
     }
     const detailsHtml = `
     <div>
     <h4>Greeting: ${typeClicked.dataset.greeting}</h4>
     <h4>School: ${archetypesSchool}</h4>
-    <h4>Study Book:</h4>
+    <h4>Study Book: ${archetypeBook}</h4>
     </div>`;
     const detailsContainer = document.querySelector("#details_container");
     detailsContainer.innerHTML = detailsHtml;
